4393f2f4eae35239fee23f6b8d81008167b25226
Naresh <ghostwriternr@gmail.com>
2017-03-25 02:41:48 +0530

    Delete useless libraries

--- public/js/controller/gyft-fetch.js
+++ public/js/controller/gyft-fetch.js
@@ -1,17 +1,19 @@
 angular.module('getTimeTable', [])
     .controller('timetableController', ['$scope', '$location', '$sce', 'TimeTables', function($scope, $location, $sce, TimeTables) {
         var rollno = $location.search().user;
-        if (rollno.length > 0) {
-            TimeTables.getTTHtml(rollno)
-                .then(function(data) {
-                    var ttfh = "'" + data.data + "'";
-                    $scope.timetableFromHtml = $sce.trustAsHtml(ttfh);
-                });
-        }
+        if (!angular.isUndefined(rollno)) {
+            if (rollno.length > 0) {
+                TimeTables.getTTHtml(rollno)
+                    .then(function(data) {
+                        var ttfh = "'" + data.data + "'";
+                        $scope.timetableFromHtml = $sce.trustAsHtml(ttfh);
+                    });
+            }
 
-        $scope.downloadTT = function() {
-            console.log("Initiating timetable download");
-            path = "/api/downloadICS/" + $scope.rollno.toLowerCase();
-            $window.location.href = path;
+            $scope.downloadTT = function() {
+                console.log("Initiating timetable download");
+                path = "/api/downloadICS/" + $scope.rollno.toLowerCase();
+                $window.location.href = path;
+            }
         }
     }]);
