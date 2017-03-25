f38df5d89f529667edb91493e03270f3f0c4041e
Naresh <ghostwriternr@gmail.com>
2017-03-26 04:55:46 +0530

    Calendar added

--- public/js/controller/calendar-fetch.js
+++ public/js/controller/calendar-fetch.js
@@ -1,7 +1,17 @@
 angular.module('calendar', [])
     .controller('calendarController', ['$scope', '$http', function($scope, $http) {
         const requesturl = 'http://139.59.35.210/';
+        const googleurl = 'https://maps.googleapis.com/maps/api/staticmap?';
         console.log("Initiating get all events");
+        var getAllMaps = function() {
+            for (var i = 0; i < $scope.successAllEvents.length; ++i) {
+                if ($scope.successAllEvents[i].placeLocation != null) {
+                    var tempUrl = 'imgUrl' + String(i);
+                    console.log(tempUrl);
+                    $scope[tempUrl] = googleurl + 'markers=' + $scope.successAllEvents[i].placeLocation.longtitude + ',' + $scope.successAllEvents[i].placeLocation.latitude + '&size=1400x400&key=AIzaSyAEgf-jv7L8DKO0h3CyMf2lHLmX5jszPyc&format=PNG';
+                }
+            }
+        }
         $scope.getAllEvents = function() {
             $http({
                 method: 'GET',
@@ -15,7 +25,8 @@ angular.module('calendar', [])
                 }
             }).then(function(response) {
                     console.log(response);
-                    $scope.successAllEvents = response.data;
+                    $scope.successAllEvents = response.data.data;
+                    getAllMaps();
                 },
                 function(response) {
                     console.log(response);
